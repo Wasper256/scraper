2cf72307376b59b03583b01893dfaec715e96836
Wasper <Wasper256@users.noreply.github.com>
2017-01-15 19:15:37 +0200

    main file, first complete version

--- main.py
+++ main.py
@@ -7,32 +7,35 @@ from selenium.webdriver.support import expected_conditions as ec
 import unicodecsv
 
 driver = webdriver.PhantomJS(service_args=['--ignore-ssl-errors=true'])
-driver.set_page_load_timeout(300)
+driver.set_page_load_timeout(30)
 
 
 def main():
-    # url_collector()
+    url_collector()
     with open("links.txt", 'r') as file:
         urllist = file.read().splitlines()
+    # rejecting URLs, that meet twice in file
     for x in urllist:
         ch = urllist.count(x)
         if ch > 1:
             urllist.remove(x)
-    # with open('output.csv', 'w') as csvfile:
-    #    fieldnames = ['Brand', 'MPN', 'URL', 'Name', 'Price', 'Stock']
-    #    writer = unicodecsv.DictWriter(csvfile, fieldnames=fieldnames)
-    #    writer.writeheader()
-    #    for y in urllist:
-    scraper()
+    # creating .csv file
+    with open('output.csv', 'wb') as csvfile:
+        fieldnames = ["Brand", "MPN", "URL", "Name", "Price", "Stock"]
+        unicodecsv.DictWriter(csvfile, fieldnames=fieldnames).writeheader()
+    # scraping URLs
+    for y in urllist:
+        scraper(y, fieldnames)
 
 
 def url_collector():
     url_iter = 1
-    print("Processing page 1 for item URLs...")
     while url_iter < 14:
         try:
-            url = "https://search.jd.com/Search?keyword=qnap&enc=utf-8&page=" + str(url_iter)
+            # loading page
+            url = "https://search.jd.com/Search?keyword=qnap&enc=utf-8" + "&page=" + str(url_iter)
             driver.get(url)
+            # activating JavaScript, that loads second part of page
             try:
                 driver.find_element_by_xpath('//*[@id="J_scroll_loading"]').click()
             except:
@@ -41,6 +44,7 @@ def url_collector():
                 WebDriverWait(driver, 10).until(ec.invisibility_of_element_located((By.ID, "J_scroll_loading")))
             finally:
                 pass
+            # collecting URLs
             soup = BeautifulSoup(driver.page_source, 'html.parser')
             g_data = soup.find_all("div", {"class": "goods-list-v2 gl-type-1 J-goods-list"})
             for item in g_data:
@@ -52,51 +56,73 @@ def url_collector():
                         with open("links.txt", 'a') as file:
                             file.write(res_link)
             url_iter = url_iter + 2
-            print("Processing page " + str((url_iter + 1) / 2) + " for item URLs...")
         except:
             pass
-    print("done")
 
 
-def scraper():
-    URL = "http://item.jd.com/10346678743.html"
-    MPN = URL.replace(".html", '').replace("http://item.jd.com/", '')
-    driver.get(URL)
+def scraper(y, fieldnames):
+    url = y
+    # collecting MPN from URL
+    mpn = url.replace(".html", '').replace("http://item.jd.com/", '')
+    # Loading page. Page is heavy that is why here i trying load page 3 times.
     try:
-        driver.find_element_by_xpath('//*[@id="detail"]/div[1]/ul/li[2]').click()
-    finally:
-        pass
+        driver.set_page_load_timeout(30)
+        driver.get(url)
+    except:
+        try:
+            driver.set_page_load_timeout(60)
+            driver.get(url)
+        except:
+            try:
+                driver.set_page_load_timeout(120)
+                driver.get(url)
+            except:
+                return()
+    # extracting price and product availability, that loaded by JavaScript
     try:
-        WebDriverWait(driver, 10).until(ec.invisibility_of_element_located((By.ID, "J-detail-content")))
-    finally:
-        pass
+        driver.find_element_by_xpath('//*[@id="detail"]/div[1]/ul/li[3]').click()
+    except:
+        try:
+            driver.find_element_by_xpath('//*[@id="detail"]/div[1]/ul/li[2]').click()
+        except:
+            driver.find_element_by_xpath('//*[@id="detail-tab-param"]').click()
+    WebDriverWait(driver, 10).until(ec.invisibility_of_element_located((By.ID, "J-detail-content")))
     try:
         driver.find_element_by_xpath('//*[@id="detail"]/div[1]/ul/li[1]').click()
-    finally:
-        pass
+    except:
+        driver.find_element_by_xpath('//*[@id="detail-tab-intro"]').click()
+    # collecting data
     soup = BeautifulSoup(driver.page_source, "html.parser")
     g_data = soup.find_all("div", {"class": "itemInfo-wrap"})
+    if not g_data:
+        g_data = soup.find_all("div", {"id": "itemInfo"})
     for item in g_data:
-        Name = (item.find_all("div", {"class": "sku-name"})[0].text)
-        Price = (item.find_all("span", {"class": "p-price"})[0].text)
+        # collecting product name
+        if (item.find_all("div", {"class": "sku-name"})):
+            name = (item.find_all("div", {"class": "sku-name"})[0].text)
+        else:
+            name = (item.find_all("div", {"id": "name"})[0].text)
+        # collecting price
+        if (item.find_all("span", {"class": "p-price"})):
+            price = (item.find_all("span", {"class": "p-price"})[0].text)
+        else:
+            price = (item.find_all("strong", {"class": "p-price"})[0].text)
+        # cheking "in Stock?"
         for a in item.find_all("a", {"id": "InitCartUrl"}):
             st = a['href']
             if st == '#none':
-                Stock = 0
+                stock = 0
             else:
-                Stock = 1
+                stock = 1
     m_data = soup.find_all("div", {"class": "p-parameter"})
+    # collecting product brand
     for item in m_data:
-        Brand = (item.find_all("a")[0].text)
-        Brand = Brand.decode('gb2312').encode('utf-8')
-    print Brand + MPN + URL + Name + Price
-    with open('output.csv', 'wb') as csvfile:
-        w = unicodecsv.writer(csvfile, encoding='utf-8-sig')
-        print Brand.encode('utf-8')
-        op = {'Brand': str(Brand).encode('utf-8'), 'MPN': str(MPN), 'URL': str(URL), 'Name': str(Name).encode('utf-8'), 'Price': str(Price), 'Stock': str(Stock)}
-        print op()
+        brand = (item.find_all("a")[0].text)
+    # writing collected data into .csv file
+    with open('output.csv', 'ab') as csvfile:
+        w = unicodecsv.DictWriter(csvfile, encoding='utf-8-sig', fieldnames=fieldnames)
+        op = {"Brand": brand, "MPN": mpn, "URL": url, "Name": name, "Price": price, "Stock": stock}
         w.writerow(op)
-
 
 if __name__ == '__main__':
     main()
